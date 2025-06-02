8d9815aca085ed8cf1dac3138ce359d18402cad2
Hkhan2712 <nguynkit3107@gmail.com>
2025-06-02 08:22:29 +0700

    Created: View of echo user

---
+++ views/user/view.php
@@ -0,0 +1,28 @@
+<?php include_once 'views/layouts/'.$this->layout.'header.php';?>
+<div class = "row row-offcanvas row-offcanvas-left">
+    <div class="col-xs-12 col-sm-9">
+        <div class="table-responsive">
+            <table class="table table-bordered table-striped">
+            <colgroup>
+                <col class="col-xs-1">
+                <col class="col-xs-7">
+            </colgroup>
+            <thead>
+                <tr>
+                <th>Field</th>
+                <th>Value</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php foreach($this->record as $field=>$value) : ?>
+                <tr>
+                <th scope="row"><?php echo $field ?></th>
+                <td><?php echo $value ?></td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include_once 'views/layouts/'.$this->layout.'footer.php';?>
